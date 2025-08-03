1000b156e44efeec593e954546bfb4199fab3ab3
JuaMedCa <juamedca@hotmail.com>
2025-08-02 22:21:38 -0600

    ya funsiono

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,6 @@ const devolucionesRoutes = require('./routes/devoluciones');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use('/autos', require('./routes/autos'));
 app.use(cors());
 app.use(express.json());
 app.use('/autos', autosRoutes);
